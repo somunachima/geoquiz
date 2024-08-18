6f1344fd5be424487a2036bac815d59088553264
Som Akudu <somakudu@gmail.com>
2024-08-18 15:50:52 +0100

    Create new android project, geoquiz

---
+++ app/src/main/java/com/project/geoquiz/QuizActivity.java
@@ -0,0 +1,15 @@
+package com.project.geoquiz;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.geoquiz.R;
+
+public class QuizActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+    }
+}
