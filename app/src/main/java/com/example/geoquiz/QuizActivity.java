d5155d827ffcafae03e7d3b8a7a780f368e2e795
Som Akudu <somakudu@gmail.com>
2024-08-19 19:00:52 +0100

    Add toast notification when true and false. Also add code to change background when true or false

--- app/src/main/java/com/example/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/geoquiz/QuizActivity.java
@@ -1,40 +1,51 @@
 package com.example.geoquiz;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class QuizActivity extends AppCompatActivity {
-
-    private Button mTrueButton;
-    private Button mFalseButton;
-
-//    QuizActivity(
-//            Button mTrueButton,
-//            Button mFalseButton
-//    ) {
-//        this.mTrueButton = mTrueButton;
-//        this.mFalseButton = mFalseButton;
-//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
-        mTrueButton = (Button) findViewById(R.id.btn_true);
+        Button mTrueButton = findViewById(R.id.btn_true);
         mTrueButton.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Check if the text is true
                 // If it is true, change the background colour to green
-                // If it is not true, change the background to red
-                // Toast notification saying True or False
+                findViewById(R.id.activity_quiz).setBackgroundColor(Color.GREEN);
+                // Toast notification saying True
+                Toast.makeText(
+                        QuizActivity.this,
+                                R.string.correct_toast,
+                                Toast.LENGTH_SHORT
+                ).show();
             }
         });
 
-        mFalseButton = (Button) findViewById(R.id.btn_false);
+        Button mFalseButton = findViewById(R.id.btn_false);
+        mFalseButton.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Check if the text is false
+                // If it is false, change the background to red
+                findViewById(R.id.activity_quiz).setBackgroundColor(Color.RED);
+                // Toast notification saying False
+                Toast.makeText(
+                        QuizActivity.this,
+                                R.string.incorrect_toast,
+                                Toast.LENGTH_SHORT
+                ).show();
+            }
+        });
     }
+
 }
